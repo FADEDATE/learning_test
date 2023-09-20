078050eb579975d9db2e60c485faeae1cd238c31
FADEDATE <2209748008@qq.com>
2023-09-20 18:16:49 +0800

    default

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import torch
+import numpy as np
 
 # def print_hi(name):
 #     # Use a breakpoint in the code line below to debug your script.
@@ -12,6 +13,17 @@ import torch
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    print(torch.randn(2, 1))
+    a = torch.tensor([i for i in range(10)])
+    b = torch.tensor([i for i in range(10)])
+    c = [i for i in range(10)]
+    d = [i for i in range(10)]
+    f = [i for i in range(10)]
+    e = np.zeros((10, 3))
+    e[:, 0] = c
+    e[:, 1] = d
+    e[:, 2] = f
+    g = e.reshape(-1, 5, 2)
+    print(e[:, [0, 1]])
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
